18c0495197a9215879ed28ad5c36b75ebfd1ed56
Markusnyy <markusngyongye@gmail.com>
2020-08-01 22:47:23 +0800

    Updated WaveSpawner

--- HGJ/Assets/Scripts/Bullet.cs
+++ HGJ/Assets/Scripts/Bullet.cs
@@ -7,9 +7,11 @@ public class Bullet : MonoBehaviour
     public Vector2 velocity;
     public float speed;
     public float rotation;
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
+        timer = 10.0f;
         transform.rotation = Quaternion.Euler(0, 0, rotation);
     }
 
@@ -17,5 +19,10 @@ public class Bullet : MonoBehaviour
     void Update()
     {
         transform.Translate(velocity * speed * Time.deltaTime);
+
+        if (timer < 0.0f)
+            Destroy(this.gameObject);
+
+        timer -= Time.deltaTime;
     }
 }
---
+++ HGJ/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public GameObject[] enemyResource;
+
+    void Start()
+    {
+        Invoke("SpawnWave1", 2.0f);
+        Invoke("SpawnWave2", 6.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public GameObject SpawnWave1()
+    {
+        // Spawn Enemies
+        GameObject enemy1 = new GameObject();
+
+        enemy1 = Instantiate(enemyResource[0], transform);
+
+        return enemy1;
+    }
+
+    public GameObject SpawnWave2()
+    {
+        // Spawn Enemies
+        GameObject enemy1 = new GameObject();
+
+        enemy1 = Instantiate(enemyResource[0], transform);
+
+        return enemy1;
+    }
+}
