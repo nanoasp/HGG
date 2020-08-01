1034759141076c136964d3a73abb1cfadbe64487
JunHui <59942852+mowjh31@users.noreply.github.com>
2020-08-01 13:58:26 +0800

    Interface

---
+++ HGJ/Assets/Scripts/interfaces.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public interface IState
+{
+    void Enter();
+
+    void Execute();
+    
+    void Exit();
+}
+
+public class StateMachine
+{
+    IState mCurrentState;
+    List<IState> allStates = new List<IState>();
+    int mCurrentStateEnum = -2;
+    
+    public void RegisterState(IState newState, int stateEnum = -1)
+    {
+        allStates.Add(newState);
+    }
+
+    public void ChangeState(int stateEnum = -1)
+    {
+        if (stateEnum == mCurrentStateEnum)
+            return;
+
+        if (mCurrentState != null)
+            mCurrentState.Exit();
+
+        mCurrentState = allStates[stateEnum];
+        mCurrentStateEnum = stateEnum;
+        mCurrentState.Enter();
+    }
+
+    public void ChangeState(IState newState, int stateEnum = -1)
+    {
+        if (stateEnum == mCurrentStateEnum)
+            return;
+
+        if (mCurrentState != null)
+            mCurrentState.Exit();
+
+        mCurrentState = newState;
+        mCurrentStateEnum = stateEnum;
+        mCurrentState.Enter();
+    }
+
+    public void Update()
+    {
+        if (mCurrentState != null) mCurrentState.Execute();
+    }
+
+    public int GetStateNumber()
+    {
+        return mCurrentStateEnum;
+    }
+}
