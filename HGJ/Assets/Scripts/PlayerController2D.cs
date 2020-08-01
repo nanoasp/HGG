79d0c3c4eae4a97ab9a74b32af3f3e2c867f96ac
nanoasp <nanoasp@gmail.com>
2020-08-01 14:23:55 +0800

    fake 2Dwata is in
    watamelon

--- HGJ/Assets/Scripts/PlayerController2D.cs
+++ HGJ/Assets/Scripts/PlayerController2D.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿/*
+ * using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -528,3 +529,4 @@ public class PlayerController2D : MonoBehaviour
 
 
 }
+*/
