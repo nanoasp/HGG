eb1c0bc224aebcb41f32868e14743d06be69bafb
JunHui <59942852+mowjh31@users.noreply.github.com>
2020-08-02 21:25:42 +0800

    Spiral Stationary

---
+++ HGJ/Assets/Scripts/SpiralBulletFormation.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiralBulletFormation : MonoBehaviour
+{
+    public GameObject mVerticalBulletPrefab;
+    public GameObject mHorizontalBulletPrefab;
+
+    public float mSpawnInterval;
+    public float mAmplitudeGrowthSpeed;
+    public float mAngleGrowthSpeed;
+
+    Transform mMyTransform;
+    float mSpawnCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mSpawnCounter = mSpawnInterval;
+        mMyTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mSpawnCounter < mSpawnInterval)
+        {
+            mSpawnCounter += Time.deltaTime;
+            return;
+        }
+        else
+            mSpawnCounter = 0.0f;
+        
+        GameObject bullet;
+        bullet = SpiralBulletFormation.Instantiate(mHorizontalBulletPrefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.Euler(0.0f, 0.0f, 0.0f)) as GameObject;
+        bullet.transform.position = mMyTransform.position;
+        SpiralBulletHorizontal sp = bullet.GetComponent<SpiralBulletHorizontal>();
+        sp.mAmplitudeGrowth = mAmplitudeGrowthSpeed;
+        sp.mAngleGrowth = mAngleGrowthSpeed;
+
+        bullet = SpiralBulletFormation.Instantiate(mHorizontalBulletPrefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.Euler(0.0f, 0.0f, 0.0f)) as GameObject;
+        bullet.transform.position = mMyTransform.position;
+        sp = bullet.GetComponent<SpiralBulletHorizontal>();
+        sp.mAmplitudeGrowth = -mAmplitudeGrowthSpeed;
+        sp.mAngleGrowth = mAngleGrowthSpeed;
+
+        bullet = SpiralBulletFormation.Instantiate(mVerticalBulletPrefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.Euler(0.0f, 0.0f, 0.0f)) as GameObject;
+        bullet.transform.position = mMyTransform.position;
+        SpiralBulletVertical spv = bullet.GetComponent<SpiralBulletVertical>();
+        spv.mAmplitudeGrowth = mAmplitudeGrowthSpeed;
+        spv.mAngleGrowth = -mAngleGrowthSpeed;
+
+        bullet = SpiralBulletFormation.Instantiate(mVerticalBulletPrefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.Euler(0.0f, 0.0f, 0.0f)) as GameObject;
+        bullet.transform.position = mMyTransform.position;
+        spv = bullet.GetComponent<SpiralBulletVertical>();
+        spv.mAmplitudeGrowth = -mAmplitudeGrowthSpeed;
+        spv.mAngleGrowth = -mAngleGrowthSpeed;
+    }
+}
---
+++ HGJ/Assets/Scripts/SpiralBulletVertical.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiralBulletVertical : MonoBehaviour
+{
+    public float mAmplitudeGrowth;
+    public float mAngleGrowth;
+
+    float mAngle = 0.0f;
+    float mAmplitude = 0.0f;
+
+    Transform mMyTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mMyTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = Mathf.Cos(mAngle) * mAmplitude;
+        float x = Mathf.Sin(mAngle) * mAmplitude;
+
+        mMyTransform.position = new Vector2(x, y);
+        mAngle += Time.deltaTime * mAngleGrowth;
+        mAmplitude += Time.deltaTime * mAmplitudeGrowth;
+    }
+}
