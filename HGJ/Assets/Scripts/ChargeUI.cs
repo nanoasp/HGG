4c4ea19f0c6bf4102cc7cf4625707826865805ca
nanoasp <nanoasp@gmail.com>
2020-08-06 10:46:41 +0800

    lv1.1

---
+++ HGJ/Assets/Scripts/ChargeUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChargeUI : MonoBehaviour
+{
+    Image box;
+    public PlayerController2D Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player").GetComponent<PlayerController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        box.color = new Color(1,1,1, 0.5f + Player.mBeamCounter / 200);
+        box.fillAmount = Player.mBeamCounter / 100;
+
+
+
+    }
+}
